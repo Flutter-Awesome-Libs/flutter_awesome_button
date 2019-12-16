d823eb16e290ce320078e5a3c92fb6384d767c83
Nemi Shah <nemishah1212@gmail.com>
2019-12-17 00:13:19 +0530

    Changes to default buttons example

--- example/lib/default_sized_buttons.dart
+++ example/lib/default_sized_buttons.dart
@@ -16,11 +16,70 @@ class DefaultSized extends StatelessWidget {
           padding: EdgeInsets.all(32),
           child: Column(
             children: <Widget>[
-              AwesomeButton(
-                text: "Click me",
-                interactMode: InteractMode.OPACITY,
-                activeOpacity: 0,
-              )
+              Row(
+                children: <Widget>[
+                  AwesomeButton(
+                    text: "Small",
+                    interactMode: InteractMode.OPACITY,
+                    activeOpacity: 0,
+                    size: ButtonSize.SMALL,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: AwesomeButton(
+                      text: "Normal",
+                      interactMode: InteractMode.OPACITY,
+                      activeOpacity: 0,
+                      backgroundColor: Colors.pinkAccent,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: AwesomeButton(
+                      text: "Large",
+                      interactMode: InteractMode.OPACITY,
+                      activeOpacity: 0,
+                      size: ButtonSize.LARGE,
+                      backgroundColor: Colors.redAccent,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    AwesomeButton(
+                      text: "Small",
+                      interactMode: InteractMode.OPACITY,
+                      activeOpacity: 0,
+                      size: ButtonSize.SMALL,
+                      disabled: true,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: AwesomeButton(
+                        text: "Normal",
+                        interactMode: InteractMode.OPACITY,
+                        activeOpacity: 0,
+                        disabled: true,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: AwesomeButton(
+                        text: "Large",
+                        interactMode: InteractMode.OPACITY,
+                        activeOpacity: 0,
+                        size: ButtonSize.LARGE,
+                        disabled: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
